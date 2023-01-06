162e6ec45d175faa3b67aa716947b2924c411060
Vip <vwetoria@gmail.com>
2023-01-07 04:54:14 +0800

    point to code directory

--- scripts/util/fs.js
+++ scripts/util/fs.js
@@ -1,7 +1,7 @@
 exports.codeFolder = (() => {
   // This path will target in GitBox root dir, will like below:
   // /private/var/mobile/Containers/Shared/AppGroup/a-hash-path/JSBox/Code/GitBox
-  const path = $file.absolutePath("../");
+  const path = $file.absolutePath("../../");
   const comps = path.split("/");
   comps.pop();
   return comps.join("/");
